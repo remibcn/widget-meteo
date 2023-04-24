c7594011a24c4185d818f0e911c087087cfe01c5
remibcn <remi.boccone@gmail.com>
2023-04-24 09:45:44 +0200

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import WidgetMeteo from '../WidgetMeteo/WidgetMeteo'
+function App() {
+  return (
+    <div>
+      <WidgetMeteo />
+      <h1>Bienvenue sur le météo widget de Rémi</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WidgetMeteo/WidgetMeteo.tsx
@@ -0,0 +1,11 @@
+function WidgetMeteo() {
+    return (
+      <div className="meteo">
+        <div className="meteo-city">Paris</div>
+        <div className="meteo-zipcode">75001</div>
+        <div className="meteo-temperature">10°C</div>
+      </div>
+  )
+}
+
+export default WidgetMeteo;
